518e8d73aa9202a071e7fbf2c4020296987d46c1
qwer123456555 <golubovskiy42@gmail.com>
2026-02-25 00:53:27 +0300

    Final version

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import { 
