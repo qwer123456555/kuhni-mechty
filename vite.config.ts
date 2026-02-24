d75c884df317311a8cbff185e336205c4417298f
qwer123456555 <golubovskiy42@gmail.com>
2026-02-25 00:58:58 +0300

    черточка и нижние плюшки

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
-    // Важно: удаляем все ручные настройки имен!
-    // Vite сам сделает правильные хэши (index-ab12cd.js)
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-  },
+    // Using default rollup options for proper hashing and caching
+  }
 });
