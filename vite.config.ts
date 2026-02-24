67b4373d59721cd6a9f9ae85f286f62ec0fba082
qwer123456555 <golubovskiy42@gmail.com>
2026-02-24 23:53:28 +0300

    Fix build config and remove api

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,24 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `script.js`,
+        chunkFileNames: `script.js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
 });
