267188b3d2a15a53597bb497c966a6841ce8bcd1
qwer123456555 <golubovskiy42@gmail.com>
2026-02-25 19:18:07 +0300

    черточка и нижние плюшки

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -235,7 +235,8 @@ export default function Quiz() {
     // Multiple chat IDs - add all recipients here
     const chatIds = [
       "536363486",  // First recipient
-      "354542692"   // Add more recipients like this
+      "354542692",
+      "536363486"   // Add more recipients like this
       // "123456789", // Third recipient
       // "987654321", // Fourth recipient
     ];
