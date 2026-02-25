71b232be7393ce35683ea82fb45967e893581cac
qwer123456555 <golubovskiy42@gmail.com>
2026-02-25 14:12:45 +0300

    черточка и нижние плюшки

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useYandexMetrika } from "./hooks/useYandexMetrika";
-import { Home } from "./pages/Home";
-import About from "./pages/About";
-import Catalog from "./pages/Catalog";
-import Gallery from "./pages/Gallery";
-import Quiz from "./pages/Quiz";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { Analytics } from "@vercel/analytics/react";
+import { Home } from "@/pages/Home";
+import Quiz from "@/pages/Quiz";
+import Catalog from "@/pages/Catalog";
+import Gallery from "@/pages/Gallery";
+import About from "@/pages/About";
+import Contacts from "@/pages/Contacts";
 
-// Обёртка для хука (хук должен быть внутри BrowserRouter)
-function MetrikaTracker() {
-  useYandexMetrika();
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return null;
 }
 
-function App() {
+export function App() {
   return (
     <BrowserRouter>
-      <MetrikaTracker /> {/* ← добавить эту строку */}
+      <ScrollToTop />
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/quiz" element={<Quiz />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/gallery" element={<Gallery />} />
-        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
